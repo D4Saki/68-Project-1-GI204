11d5b937af27c8a09b0df4767a512100fbc6e68a
D4Saki <thkvtpa@gmail.com>
2026-03-17 15:29:30 +0700

    d

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -5,7 +5,7 @@ public class CameraFollow : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        public
     }
 
     // Update is called once per frame
